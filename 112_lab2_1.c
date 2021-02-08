d57061cc070447f13df48acbdfde7c854d876a3a
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-02-08 16:54:33 +0700

    Grading

--- 112_lab2_1.c
+++ 112_lab2_1.c
@@ -10,5 +10,29 @@ int main()
     {
         printf("A");
     }
+    else if(total>=75&&total<=79)
+    {
+        printf("B+");
+    }
+    else if(total>=70&&total<=74)
+    {
+        printf("B");
+    }
+    else if(total>=65&&total<=69)
+    {
+        printf("C");
+    }
+    else if(total>=60&&total<=64)
+    {
+        printf("C");
+    }
+    else if(total>=55&&total<=59)
+    {
+        printf("D+");
+    }
+    else if(total>=50&&total<=54)
+    {
+        printf("D");
+    }
 
 }
