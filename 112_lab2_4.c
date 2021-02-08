2801f8cb87196e99286be8536177c18b03ddd96f
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-02-08 17:40:08 +0700

    Emoji

--- 112_lab2_4.c
+++ 112_lab2_4.c
@@ -1,12 +1,9 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
     int date;
     char name,hand1='/',hand2=92;
-    printf("Enter name:");
     scanf("%c" ,&name);
-    printf("Enter Birth Date:");
     scanf("%d" ,&date);
     int i;
     i=date%10;
