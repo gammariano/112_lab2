a307ff6868503b70e53c8344b6dcf83a1dfa0fd3
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-02-08 19:52:23 +0700

    First Day

--- 112_lab2_5.c
+++ 112_lab2_5.c
@@ -1,7 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int day,month;
+    int day,month,i=1,num;
     scanf("%d" ,&day);
     scanf("%d" ,&month);
+    switch(month)
+    {
+        case 1 :
+        case 3 :
+        case 5 :
+        case 7 :
+        case 8 :
+        case 10 :
+        case 12 : num=31; break;
+        case 2 : num=28; break;
+        case 4 :
+        case 6 :
+        case 9 :
+        case 11 : num=30; break;
+    }
 }
