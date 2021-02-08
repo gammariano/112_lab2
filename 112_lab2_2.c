b2381c082209bce6a8b26a380ebe005d91190c22
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-02-08 17:52:28 +0700

    Promotion

--- 112_lab2_2.c
+++ 112_lab2_2.c
@@ -2,7 +2,7 @@
 int main()
 {
     char promo;
-    float time,a=199,b=299,i;
+    float time,a=199,b=299,i,sec;
     scanf("%c" ,&promo);
     scanf("%.2f" ,&time);
     if(promo=='A')
@@ -10,6 +10,14 @@ int main()
         if(time<=200)
         {
             printf("%.2f" ,a);
+        }
+        else if(time>200)
+        {
+            time-=200;
+            sec=time*60;
+            i=sec*(3.00/60.00);
+            a+=i;
+
         }
     }
 }
