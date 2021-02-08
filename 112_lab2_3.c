99f2da40708c13e85795b7a583f687e1d9f7cfc0
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-02-08 19:09:35 +0700

    ABC

--- 112_lab2_3.c
+++ 112_lab2_3.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
-    char a2,b2,c2;
+    int a,b,c,count;
+    char a1[5];
+    int i,b1[5];
     scanf("%d %d %d" ,&a,&b,&c);
     scanf("%c%c%c" ,&a2,&b2,&c2);
 
