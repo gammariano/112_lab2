bd9087dbbbf816499d16785237358034cbde6304
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-02-08 16:52:48 +0700

    Grading

--- 112_lab2_1.c
+++ 112_lab2_1.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    double a,b,c;
-    char grade;
+    double a,b,c,total;
     scanf("%ld" ,&a);
     scanf("%ld" ,&b);
     scanf("%ld" ,&c);
+    total=a+b+c;
+    if(total>=80&&total<=100)
+    {
+        printf("A");
+    }
 
 }
